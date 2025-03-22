b28a622d916afa6a6c425c092f855aea7e85d7e9
HaydenSiev <149506008+HaydenSiev@users.noreply.github.com>
2025-03-22 15:17:52 -0600

    ficed note callback

--- Pi_Code/voice_geek.py
+++ Pi_Code/voice_geek.py
@@ -8,12 +8,15 @@ import pvrhino
 import pvleopard
 
 class VoiceGeek:
-    def __init__(self, mode_switcher_callback=None, db_check_interval=30, db_alert_callback=None, db_threshold=90):
+    def __init__(self, mode_switcher_callback=None, db_check_interval=30, db_alert_callback=None, db_threshold=90, note_callback=None):
         # Store callback function to switch modes
         self.mode_switcher_callback = mode_switcher_callback
         self.db_check_interval = db_check_interval
         self.db_alert_callback = db_alert_callback
         self.db_threshold = db_threshold
+        
+        # Store the note callback
+        self.note_callback = note_callback
         
         # Initialize Porcupine
         self.porcupine = None
