29e94f2bfdd48dbc301bd82518ab0d07a56472b4
Hayden <h.seivewright@gmail.com>
2025-03-31 09:49:33 -0600

    basic mqtt test connected

--- Pi_Code/Examples/mqtt_test.py
+++ Pi_Code/Examples/mqtt_test.py
@@ -2,9 +2,14 @@ import paho.mqtt.client as mqtt
 
 # Callback when the client receives a CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    # Subscribe to topic (you can change "test/topic" to whatever topic you want)
-    client.subscribe("test/topic")
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+        # Subscribe to topic (you can change "test/topic" to whatever topic you want)
+        client.subscribe("#")
+        connected = True
+    else:
+        print("Failed to connect, return code %d\n", rc)
+    
 
 # Callback when a message is received from the server
 def on_message(client, userdata, msg):
@@ -17,14 +22,18 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+con = False
+
 # Connect to the broker (modify these parameters according to your broker)
 broker_address = "broker.hivemq.com"  # This is a public test broker
 port = 1883
 # If your broker requires authentication, uncomment and modify these lines:
 # client.username_pw_set("your_username", "your_password")
 
+
 # Connect to the broker
 client.connect(broker_address, port, 60)
+
 
 # Start the loop to process network traffic
 client.loop_forever()
